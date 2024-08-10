c8f057a5678db13db1d5ba0606789363237e33e8
reetam486itb <reetam486itb@gmail.com>
2024-08-10 17:46:27 +0530

    before part 2

--- mongodb_practice/routes/index.js
+++ mongodb_practice/routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const userModel= require("./users");
+const { route } = require('../app');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 // router.get("/create", async (req, res)=>{
 //   const entry_1=await userModel.create({
@@ -17,14 +18,42 @@ router.get('/', function(req, res, next) {
 //   res.send(entry_1);
 // })
 
-router.get("/retrive",async(req,res)=>{
-  const res_user=await userModel.findOne({username:"reetam"});
-  res.send(res_user); 
-})
+// router.get("/retrive",async(req,res)=>{
+//   const res_user=await userModel.findOne({username:"reetam"});
+//   res.send(res_user); 
+// })
 
 // router.get("/delete",async(req,res)=>{
 //   const del_user=await userModel.findOneAndDelete({username:"reetam"});
 //   res.send(del_user); 
 // })
+
+// router.get("/", (req,res)=>{
+ 
+//   req.session.ban= true;
+//   res.send("banning in process")
+
+// })
+
+// router.get("/check_banned", (req,res)=>{
+//   // console.log(req.session);
+//   if(req.session.ban===true){ res.send("u are banned")}
+//   res.send(".")
+// })
+
+router.get("/", (req, res)=>{
+  res.cookie("age", 25);
+  res.send("hello")
+})
+
+router.get("/read", (req, res)=>{
+  console.log(req.cookies);
+  res.send("read")
+})
+
+router.get("/delete", (req,res)=>{
+  res.clearCookie("age");
+  res.send("deleted")
+})
 
 module.exports = router;
